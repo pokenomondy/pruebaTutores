ec5b49600e49a2c91577e4ee9b7fdb3a430c3e37
pokenomondy <72961262+pokenomondy@users.noreply.github.com>
2025-01-24 21:33:07 -0500

    v 1.4.5 v3

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1656035813"
+    serviceWorkerVersion: "3153867143"
   }
 });
